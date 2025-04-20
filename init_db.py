e3143ccf0be25f99cb2687a1e9bfe9d6f73acdd1
kudoff <vsevolod6588@gmail.com>
2025-04-20 03:01:51 +0300

    Changed files

--- init_db.py
+++ init_db.py
@@ -1,6 +1,6 @@
 from sqlmodel import SQLModel
 from database import engine
-import models  # обязательно импортировать, чтобы SQLModel "видел" их
+from models import *
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
