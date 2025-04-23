d4ed147350c758fb59fae55607a051073ee1289f
kudoff <vsevolod6588@gmail.com>
2025-04-23 04:36:14 +0300

    Updated /users/delete in API

--- api/users.py
+++ api/users.py
@@ -1,16 +1,18 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel import Session, select
-from models import User
+from models import User, Request
 from database import engine
 from api.auth import get_current_user
 
 router = APIRouter(prefix="/users", tags=["Users"])
+
 
 @router.get("/")
 def get_users(current_user: User = Depends(get_current_user)):
     with Session(engine) as session:
         users = session.exec(select(User)).all()
         return users
+
 
 @router.get("/{user_id}")
 def get_user_by_id(user_id: int, current_user: User = Depends(get_current_user)):
@@ -19,3 +21,22 @@ def get_user_by_id(user_id: int, current_user: User = Depends(get_current_user))
         if not user:
             raise HTTPException(status_code=404, detail="User not found")
         return user
+
+
+@router.delete("/user/delete")
+def delete_user(current_user: User = Depends(get_current_user)):
+    with Session(engine) as session:
+        active_requests = session.exec(
+            select(Request).where(Request.user == current_user.id)
+        ).all()
+        if active_requests:
+            raise HTTPException(
+                status_code=400,
+                detail="Невозможно удалить аккаунт — у вас есть активные заявки.",
+            )
+
+        user = session.get(User, current_user.id)
+        if user:
+            session.delete(user)
+            session.commit()
+        return {"message": "Аккаунт удален"}
