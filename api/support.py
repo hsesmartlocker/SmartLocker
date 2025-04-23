4276dec4765e60eee02b8dcaf29b3708725d8c61
kudoff <vsevolod6588@gmail.com>
2025-04-23 05:12:29 +0300

    Updated /support in API

--- api/support.py
+++ api/support.py
@@ -1,28 +1,32 @@
-from fastapi import APIRouter, Depends, HTTPException, status, Body
-from sqlmodel import Session
+from fastapi import APIRouter, Depends, HTTPException, status, Request
 from api.auth import get_current_user
-from database import engine
 from models import User
 from utils.email_sender import send_support_message
 
 router = APIRouter(prefix="/support", tags=["Support"])
 
-@router.post("/", response_model=dict)
-def send_support_request(
-    data: dict = Body(...),
-    current_user: User = Depends(get_current_user)
+@router.post("/")
+async def send_support_request(
+    request: Request,
+    current_user: User = Depends(get_current_user),
 ):
-    user_email = current_user.email
+    data = await request.json()
     message = data.get("message")
 
     if not message or not message.strip():
         raise HTTPException(status_code=400, detail="Пустое сообщение обращения")
 
+    # Пытаемся отправить письмо
     try:
-        send_support_message(user_email, message)
-        return {"message": "Обращение успешно отправлено"}
+        send_support_message(
+            user_email=current_user.email,
+            message=message
+        )
     except Exception as e:
+        print(f"[email error] Ошибка при отправке письма: {e}")
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail=f"Не удалось отправить обращение: {str(e)}"
+            detail="Не удалось отправить обращение"
         )
+
+    return {"message": "Обращение успешно отправлено"}
